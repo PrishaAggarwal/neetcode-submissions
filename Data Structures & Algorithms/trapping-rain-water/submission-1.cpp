819c19de973becc5e6f36dbb8452256517a8c11c
Prisha Aggarwal <142911764+PrishaAggarwal@users.noreply.github.com>
2026-06-10 05:43:38 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        /*int n=height.size();
+        int left_max=0;
+        int right_max=0;
+        int k=0; //current index we are scanning
+        int total_water=0;
+        int water=0;
+        for(k=0;k<n-1;k++){
+            left_max=0;
+            right_max=0;
+            int i=0;
+            int j=0;
+            //for left side
+            for(i=0;i<k;i++){
+                left_max=max(left_max,height[i]);
+            }
+            for(j=k;j<n;j++){
+                right_max=max(right_max,height[j]);
+            }
+            water=min(left_max,right_max)-height[k]; //just in case k height is bigger then water cant be negative
+            if(water>0)
+                total_water=total_water+water;
+        }
+        return total_water; */
+
+        //LOGIC 2 THAT IS ACTUALLY USING POINTERS AND WE ARE MOVING FROM BOTH ENDS
+        int i=0;
+        int j=height.size()-1;
+        int max_l=height[i];
+        int max_r=height[j];
+        int water=0;
+        int total_water=0;
+        while(i<j){
+            if(max_l<max_r){
+                i++;
+                water=max_l-height[i];
+                max_l=max(max_l,height[i]);
+                if(water>0)
+                    total_water+=water;
+            }
+            else if(max_l>max_r){
+                j--;
+                water=max_r-height[j];
+                max_r=max(max_r,height[j]);
+                if(water>0)
+                    total_water+=water;
+            }
+            else{
+                i++;
+                water=max_l-height[i];
+                max_l=max(max_l,height[i]);
+                if(water>0)
+                    total_water+=water;
+            }
+        }
+        return total_water;
+    }
+};
